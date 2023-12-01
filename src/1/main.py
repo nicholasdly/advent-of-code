af73a1049125843b82fbda0e771cbb201b79d7ba
Nicholas Ly <nichdly@gmail.com>
2023-12-01 12:34:37 -0600

    Added reuseable input file reading functions

--- src/1/main.py
+++ src/1/main.py
@@ -1,16 +1,26 @@
+import os
 import time
 
-def part_one():
+def part_one(data: str):
     return 2
 
-def part_two():
+def part_two(data: str):
     return 4
 
-def main():
-    print("Hello world!")
+def get_input() -> str:
+    input_path = os.path.join(os.path.dirname(__file__), "input/input.txt")
+    with open(input_path) as file:
+        return file.read()
 
 if __name__ == "__main__":
+    data = get_input()
+
     t1 = time.perf_counter()
-    main()
+    print(part_one(data))
+    t2 = time.perf_counter()
+    print(f"Execution time: {t2 - t1:0.5f} seconds.")
+
+    t1 = time.perf_counter()
+    print(part_two(data))
     t2 = time.perf_counter()
     print(f"Execution time: {t2 - t1:0.5f} seconds.")
--- src/1/sample_test.py
+++ src/1/sample_test.py
@@ -1,7 +1,15 @@
+import pytest
+import os
 from main import part_one, part_two
 
-def test_part_one():
-    assert part_one() == 2
+@pytest.fixture
+def sample_input() -> str:
+    input_path = os.path.join(os.path.dirname(__file__), "input/sample_input.txt")
+    with open(input_path) as file:
+        return file.read()
 
-def test_part_two():
-    assert part_two() == 4
+def test_part_one(sample_input: str):
+    assert part_one(sample_input) is None
+
+def test_part_two(sample_input: str):
+    assert part_two(sample_input) is None
